617976e5e0038d611e8bbd33efdfd31b124092a4
Thomas Christopher <thomas.j.christopher@gmail.com>
2018-12-11 17:59:28 +0000

    Implement for Integers

--- lib/raidzklart_palindrome.rb
+++ lib/raidzklart_palindrome.rb
@@ -1,16 +1,29 @@
 require "raidzklart_palindrome/version"
 
-class String
+module RaidzklartPalindrome
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.scan(/[a-z]/i).join.downcase
+      self.to_s.scan(/[a-z]?\d?/i).join.downcase
     end
+  
+end
+
+class String
+  include RaidzklartPalindrome
+end
+
+class Integer
+  include RaidzklartPalindrome
 end
